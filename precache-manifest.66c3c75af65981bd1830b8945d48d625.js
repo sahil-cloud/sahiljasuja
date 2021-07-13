06217c0a4b28c0905c6750cbf1c08aa3c356535a
sahil-cloud <sahiljasuja666@gmail.com>
2021-07-13 17:59:05 +0530

    Updates

---
+++ precache-manifest.66c3c75af65981bd1830b8945d48d625.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34293dced7691b7bbd110d7fe40df35",
+    "url": "/sahiljasuja/index.html"
+  },
+  {
+    "revision": "c8f22164b1b72137dbc3",
+    "url": "/sahiljasuja/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "5ce76d43012c80a2e9ee",
+    "url": "/sahiljasuja/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c8f22164b1b72137dbc3",
+    "url": "/sahiljasuja/static/js/2.082f7e40.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/sahiljasuja/static/js/2.082f7e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce76d43012c80a2e9ee",
+    "url": "/sahiljasuja/static/js/main.bcb7ddf2.chunk.js"
+  },
+  {
+    "revision": "0ab533b7628c9e57ffed",
+    "url": "/sahiljasuja/static/js/runtime-main.8f3e773f.js"
+  },
+  {
+    "revision": "5e747b0564a422ec03827de4d1fe7c3e",
+    "url": "/sahiljasuja/static/media/aa1.5e747b05.jpg"
+  },
+  {
+    "revision": "62b946d7a6f148e642d87318f0c67a39",
+    "url": "/sahiljasuja/static/media/ai.62b946d7.jpg"
+  },
+  {
+    "revision": "41e54110e212d7df7981e01026351c70",
+    "url": "/sahiljasuja/static/media/avi4.41e54110.jpg"
+  },
+  {
+    "revision": "5bb1b013aa8640af00377df6447414e6",
+    "url": "/sahiljasuja/static/media/aws.5bb1b013.jpg"
+  },
+  {
+    "revision": "4e24f0b17c4bf9928aebd92940be5110",
+    "url": "/sahiljasuja/static/media/google.4e24f0b1.jpg"
+  },
+  {
+    "revision": "0751b97277d531712d42e7c783af0156",
+    "url": "/sahiljasuja/static/media/ibm.0751b972.jpg"
+  },
+  {
+    "revision": "7f94451338cb0de9b6503b36d915fa7a",
+    "url": "/sahiljasuja/static/media/isb.7f944513.jpg"
+  },
+  {
+    "revision": "bb8c326bca3daf500e26918f9f044674",
+    "url": "/sahiljasuja/static/media/micro.bb8c326b.jpg"
+  }
+]);
